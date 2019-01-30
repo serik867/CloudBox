0d72cc34fc50e3364280d64483416d39c39a6546
Serdar Durbayev <durbayevserdar@hotmail.com>
2019-01-30 13:08:56 -0500

    homepage fix and add table and CPU,Memory,GPU buttons

--- cloudbox/cloudbox.py
+++ cloudbox/cloudbox.py
@@ -6,28 +6,80 @@ import bson
 
 app = Flask(__name__)
 
+results=[]
+
 aws_regions = { 
-		"Asia Pasific (Mumbai)":"ap-south-1", 
-		"Asia Pasific (Seoul)":"ap-northeast-2",
-		"Asia Pasific (Singapore)":"ap-southeast-1",
-		"Asia Pacific (Sydney)":"ap-southeast-2",
-		"Asia Pacific (Tokyo)":"ap-northeast-1", 
-		"Canada (Central)":"ca-central-1",
-		"EU (Frankfurt)":"eu-central-1",
-		"EU (Ireland)":"eu-west-1", 
-		"EU (London)":"eu-west-2", 
-		"EU (Paris)":"eu-west-3", 
-		"EU (Stockholm)":"eu-north-1",
-		"South America (Sao Paulo)":"sa-east-1", 
-		"US East (N.Virginia)":"us-east-1",
-		"US East (Ohio)":"us-east-2", 
-		"US West (N.California)":"us-west-1", 
+		"India":"ap-south-1", 
+		"S.Korea":"ap-northeast-2",
+		"Singapore":"ap-southeast-1",
+		"Sydney":"ap-southeast-2",
+		"Tokyo":"ap-northeast-1", 
+		"Canada":"ca-central-1",
+		"Frankfurt":"eu-central-1",
+		"Ireland":"eu-west-1", 
+		"London":"eu-west-2", 
+		"France":"eu-west-3", 
+		"Finland":"eu-north-1",
+		"Brazil":"sa-east-1", 
+		"Virginia":"us-east-1",
+		"Ohio":"us-east-2", 
+		"California":"us-west-1", 
 		"Oregon":"us-west-2"
 				}
+azure_regions = {
+	"Sydney": "australiaeast",
+	#"Sydney": "australiasoutheast",
+	"Brazil": "brazilsouth",
+	"Canada": "canadacentral",
+	"Canada": "canadaeast",
+	"India": "centralindia",
+	"Iowa": "centralus",
+	"China": "eastasia",
+	"Virginia": "eastus",
+	"S.Carolina": "eastus2",
+	"France": "francecentral",
+	"Tokyo": "japaneast",
+	#"Tokyo": "japanwest",
+	"S.Korea": "koreacentral",
+	#"S.Korea": "koreasouth",
+	"Ohio": "northcentralus",
+	"Ireland": "northeurope",
+	#"Ohio": "southcentralus",
+	#"India": "southindia",
+	"Singapore": "southeastasia",
+	"London": "uksouth",
+	#"London": "ukwest",
+	#"Iowa": "westcentralus",
+	"Frankfurt": "westeurope",
+	#"India": "westindia",
+	"Oregon": "westus",
+	"California": "westus2"
+		}
+google_regions = {
+	"China": "asia-east2",
+	"Mumbai": "asia-south1",
+	"Singapore": "asia-southeast1",
+	"Sydney": "australia-southeast1",
+	"Taiwan": "asia-east1",
+	"Tokyo": "asia-northeast1",
+	"Canada":"northamerica-northeast1",
+	"Belgium":"europe-west1",
+	"Finland":"europe-north1",
+	"Frankfurt":"europe-west3",
+	"London":"europe-west2",
+	"Netherlands":"europe-west4",
+	"Brazil":"southamerica-east1",
+	"Iowa":"us-central1",
+	"Virginia":"us-east4",
+	"S.Carolina":"us-east1",
+	"California":"us-west2",
+	"Oregon":"us-west1"
+		}
 
-locations = [ 'Oregon', 'California', 'Virginia', 'S.California', 'Illinois', 'Ohio',
+locations = [ 'Oregon', 'California', 'Virginia', 'S.Carolina', 'Ohio',
 			'Iowa', 'India', 'Tokyo', 'S.Korea', 'Sigapore', 'Sydney', 'Ireland', 'London',
-			'Frankfurt', 'Netherland', 'France','Finland','Canada','China','Brazil' ]
+			'Frankfurt', 'Netherlands', 'France','Finland','Canada','China','Brazil' ]
+purposes = ['general', 'computer_opt','memory_opt', 'accelerated_comp','storage_opt']
 
 def connect_to_mongodb(collection_name):
 	client = pymongo.MongoClient()
@@ -45,49 +97,120 @@ def query_to_list(results):
 			'cpusPerVm':result['cpusPerVm'],
 			'memPerVm':result['memPerVm'],
 			'gpusPerVm':result['gpusPerVm'],
-			'ntwPerf':result['ntwPerf'],
-			'ntwPerfCategory':result['ntwPerfCategory']
+			'provider':result['provider']
 			})
 	return result_list
 
-def find_location(regions,location):
-	for key,value in regions.items():
-		if key == location:
-			return value
-		else:
-			pass
+#find zone from list
+def find_location(regions,location): 
+    if location in regions.keys(): 
+        return regions[location] 
+    else: 
+        return None
 
 def aws_search_by_location(location):
 	client = pymongo.MongoClient()
 	db = client['test-database']
 	col = db['aws']
-
-	places = col.find({'zone':location}).sort('cpusPerVm', 1)
+	places = col.find({'zone':location})
 	return places
 
-_list = query_to_list(aws_search_by_location('ap-northeast-2'))
+def azure_search_by_location(location):
+	client = pymongo.MongoClient()
+	db = client['test-database']
+	col = db['azure']
+	places = col.find({'zone':location})
+	return places
+
+def google_search_by_location(location):
+	client = pymongo.MongoClient()
+	db = client['test-database']
+	col = db['google']
+	places = col.find({'zone':location})
+	return places
+
+def query_to_cpu_ordered_list(_query,name):
+	_list = []
+
+	for item in _query:
+		number = int(item[name])
+
+		if number not in _list:
+			_list.append(number)
+		else:
+			pass
+	_list.sort()
+	return _list
+
+
 
 
 @app.route("/", methods = ['GET', 'POST'])
 @app.route("/index",methods = ['GET', 'POST'])
 @app.route("/home", methods = ['GET', 'POST'])
 def homepage():
-	
+	return render_template('home.html', locations=locations, purposes = purposes)
 
-		return render_template('home.html', locations=locations)
-
-@app.route("/<location>", methods = ['GET', 'POST'])
+@app.route("/<location>", methods = ['GET','POST'])
 def home_loc(location):
+	results.clear()
+
+	if location != None:
+		region_aws = find_location(aws_regions,location)
+		region_azure = find_location(azure_regions,location)
+		region_google = find_location(google_regions,location)
+		
+		if region_aws:
+			results.extend(query_to_list(aws_search_by_location(region_aws)))
+		if region_azure:
+			results.extend(query_to_list(azure_search_by_location(region_azure)))
+		if region_google:
+			results.extend(query_to_list(google_search_by_location(region_google)))
+
 	
-	print(location)
-	region = find_location(aws_regions,location)
-	results = query_to_list(aws_search_by_location(region))
+	cpu_list = query_to_cpu_ordered_list(results,'cpusPerVm')
+	mem_list = query_to_cpu_ordered_list(results,'memPerVm')	
+	gpu_list = query_to_cpu_ordered_list(results,'gpusPerVm')
+	
+	return render_template('home.html', locations=locations, results = results, purposes=purposes,
+	cpu_list=cpu_list, mem_list = mem_list, gpu_list=gpu_list, 	message=location)
 
-	return render_template('home.html', locations=locations, results = results)
+@app.route("/<location>/<purpose>")
+def location_purpose(location,purpose):
 
+	#print("{}, {} ".format(location,purpose))
+	return "{}, {}".format(location,purpose)
+
+
+
+
+# @app.route("/providers",methods =['GET','POST'])
+# def get_button_data():
+	
+# 	if request.method == 'POST':
+		
+# 		if request.form['submit_button'] == 'AWS':
+# 			#print(global_location)
+# 			print('aws')
+# 			print(results)
+# 		elif request.form['submit_button'] == 'AZURE':
+# 			print('azure')
+# 		elif request.form['submit_button'] == 'GOOGLE':
+# 			print('google')
+# 		else:
+# 			pass
+
+# 		return "DONE"
+# 	else:
+# 		return "Something Wrong"
+	
+	
+
+	
 
 @app.route('/result')
 def result():
+
 	return render_template('result.html')
 
 
